f660668b49b3839a343574b5143606ab57370e4e
James Cobbett <james.cobbett@tearfund.org>
2016-06-01 11:44:41 +0100

    Family app

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -1,3 +1,18 @@
+function onDeviceReady() {
+  directToHome();
+}
+
+function goToStory(id){
+  $(':mobile-pagecontainer').pagecontainer('change', 'stories/'+id+'/page1.html', {
+        //  beforeshow: function( event, ui ) {alert('test')}
+        transition: 'fade',
+        changeHash: true,
+        reverse: false,
+        showLoadMsg: true,
+        reload: true
+    });
+}
+
 function directToHome(){
 	$(':mobile-pagecontainer').pagecontainer('change', 'home.html', {
         //  beforeshow: function( event, ui ) {alert('test')}
@@ -9,44 +24,44 @@ function directToHome(){
     });
 }
 
-function goToStory(id){
-	// $(':mobile-pagecontainer').pagecontainer('change', 'story.html', {
- //        //  beforeshow: function( event, ui ) {alert('test')}
- //        transition: 'fade',
- //        changeHash: true,
- //        reverse: false,
- //        showLoadMsg: true,
- //        reload: true,
- //        id: id
- //    });
+// function goToStory(id){
+// 	// $(':mobile-pagecontainer').pagecontainer('change', 'story.html', {
+//  //        //  beforeshow: function( event, ui ) {alert('test')}
+//  //        transition: 'fade',
+//  //        changeHash: true,
+//  //        reverse: false,
+//  //        showLoadMsg: true,
+//  //        reload: true,
+//  //        id: id
+//  //    });
 
-	var url = "http://localhost:8888/tf-stories-wp/wp-json/wp/v2/posts/"+id;
-    console.log(url);
-    $.getJSON(url, function(result){
-    	console.log(result['_jc_stories']);
-    	$('.story-title').append(result['title']['rendered']);
-    	var page = 1;
-        $.each(result['_jc_stories'], function(i, field){
-        	console.log(field);
-            var html;
-            html = "<div data-role='page' id='page-" + page +"' class='page page-" + page +"' style='background-image: url(" + field['page_image'] + ")'>";
-                html += "<div class='page-content'>" + field['page_text'];
-                	html += "<a href='#' onclick='nextPage(" + page + ")' class='next-page'>Next page</a>";
-                html += "</div>";
-            html += "</div>";
-            page++;
-            $('body').append(html)
-        });
-        $(':mobile-pagecontainer').pagecontainer('change', '#page-1', {
-	       //  beforeshow: function( event, ui ) {alert('test')}
-	       transition: 'flip',
-	       changeHash: true,
-	       reverse: false,
-	       showLoadMsg: true,
-	       reload: true,
-	   });
-    });
-}
+// 	var url = "http://localhost:8888/tf-stories-wp/wp-json/wp/v2/posts/"+id;
+//     console.log(url);
+//     $.getJSON(url, function(result){
+//     	console.log(result['_jc_stories']);
+//     	$('.story-title').append(result['title']['rendered']);
+//     	var page = 1;
+//         $.each(result['_jc_stories'], function(i, field){
+//         	console.log(field);
+//             var html;
+//             html = "<div data-role='page' id='page-" + page +"' class='page page-" + page +"' style='background-image: url(" + field['page_image'] + ")'>";
+//                 html += "<div class='page-content'>" + field['page_text'];
+//                 	html += "<a href='#' onclick='nextPage(" + page + ")' class='next-page'>Next page</a>";
+//                 html += "</div>";
+//             html += "</div>";
+//             page++;
+//             $('body').append(html)
+//         });
+//         $(':mobile-pagecontainer').pagecontainer('change', '#page-1', {
+// 	       //  beforeshow: function( event, ui ) {alert('test')}
+// 	       transition: 'flip',
+// 	       changeHash: true,
+// 	       reverse: false,
+// 	       showLoadMsg: true,
+// 	       reload: true,
+// 	   });
+//     });
+// }
 
 function nextPage(page){
 	page++;
