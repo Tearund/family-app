162c96a28b2ed28bd95320e7cce4d4203abc7f21
James Cobbett <MacEnroe@Jamess-iMac.home>
2016-06-08 14:07:29 +0100

    story app

--- www/js/reveal.js
+++ www/js/reveal.js
@@ -24,3 +24,51 @@ $(document).on("pageshow",function(event,data){
 	  reveal($(this));
 	});
 });
+
+$(document).on("pageshow",function(event,data){
+  $('.tooltip-parent').bind('tap', function(){
+  		$('.tooltip--'+$(this).data('id')).show();
+	});
+});
+
+$(document).on("pageshow",function(event,data){
+  $('.bird').bind('tap', function(){
+  		var element = $(this);
+  		var timer = 500;
+		$(this).css('background-image','url("../../img/story1/'+element.data('id')+'-fly.png")');
+		//$(this).hide().delay(400).sh
+		setTimeout(function(){
+			console.log(element);
+			element.css('background-image','url("../../img/story1/'+element.data('id')+'.png")'); 
+		}, timer, element);
+		setTimeout(function(){
+			console.log(element);
+			element.css('background-image','url("../../img/story1/'+element.data('id')+'-fly.png")'); 
+		}, timer*2, element);
+		setTimeout(function(){
+			console.log(element);
+			element.css('background-image','url("../../img/story1/'+element.data('id')+'.png")'); 
+		}, timer*3, element);
+		setTimeout(function(){
+			console.log(element);
+			element.css('background-image','url("../../img/story1/'+element.data('id')+'-fly.png")'); 
+		}, timer*4, element);
+		setTimeout(function(){
+			console.log(element);
+			element.css('background-image','url("../../img/story1/'+element.data('id')+'.png")'); 
+		}, timer*5, element);
+		setTimeout(function(){
+			console.log(element);
+			element.css('background-image','url("../../img/story1/'+element.data('id')+'-fly.png")'); 
+		}, timer*6, element);
+		setTimeout(function(){
+			console.log(element);
+			element.css('background-image','url("../../img/story1/'+element.data('id')+'.png")'); 
+		}, timer*7, element);
+	});
+});
+
+// function tooltip(element){
+// 	console.log($('.tooltip--'+element));
+// 	$('.tooltip--'+element).show();
+// }
